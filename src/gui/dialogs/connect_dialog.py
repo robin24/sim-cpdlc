46370c74b7dd88096da4e3ed0ca7739183e91647
Robin Kipp <robin@robin-kipp.net>
2025-03-28 14:39:13 +0100

    Connect dialog: Hide Logon code field when logon code is found in config file.

--- src/gui/dialogs/connect_dialog.py
+++ src/gui/dialogs/connect_dialog.py
@@ -81,9 +81,9 @@ class ConnectDialog(wx.Dialog):
 
         vbox.Add(self.callsign_text, 0, wx.ALL | wx.EXPAND, 5)
 
-        # Logon code field - always show it, but populate based on selection
-        logon_code_label = wx.StaticText(self, label="Logon code:")
-        vbox.Add(logon_code_label, 0, wx.ALL, 5)
+        # Logon code field - create controls but manage visibility later
+        self.logon_code_label = wx.StaticText(self, label="Logon code:")
+        vbox.Add(self.logon_code_label, 0, wx.ALL, 5)
         self.logon_code_text = wx.TextCtrl(self)
         vbox.Add(self.logon_code_text, 0, wx.ALL | wx.EXPAND, 5)
 
@@ -108,8 +108,33 @@ class ConnectDialog(wx.Dialog):
         self.callsign_text.Bind(wx.EVT_TEXT, self.on_text_change)
         self.logon_code_text.Bind(wx.EVT_TEXT, self.on_text_change)
 
+        # Update logon code field visibility based on initial network selection
+        self.update_logon_code_visibility()
+
         # Check if fields are valid on initialization
         self.on_text_change(None)
+
+    def update_logon_code_visibility(self):
+        """
+        Update the visibility of the logon code field based on the selected network.
+        Hide the field if a logon code exists for the selected network.
+        """
+        selection = self.network_radio_box.GetSelection()
+
+        # Check if a logon code exists for the selected network
+        has_logon_code = False
+        if selection == 0 and self.saved_sayintentions_logon_code:  # SayIntentions
+            has_logon_code = True
+        elif selection == 1 and self.saved_hoppie_logon_code:  # Hoppie
+            has_logon_code = True
+
+        # Show or hide the logon code field based on whether a logon code exists
+        self.logon_code_label.Show(not has_logon_code)
+        self.logon_code_text.Show(not has_logon_code)
+
+        # Refresh the layout to account for the visibility change
+        self.Layout()
+        self.Fit()
 
     def on_network_selection(self, event):
         """
@@ -123,20 +148,34 @@ class ConnectDialog(wx.Dialog):
         else:  # Hoppie
             self.logon_code_text.SetValue(self.saved_hoppie_logon_code)
 
+        # Update logon code field visibility
+        self.update_logon_code_visibility()
+
         # Update button state
         self.on_text_change(None)
 
     def on_text_change(self, _):
         """
-        Enable the OK button if both callsign and logon code are provided.
+        Enable the OK button if required fields are valid.
+        If logon code field is visible, both callsign and logon code must be valid.
+        If logon code field is hidden, only callsign needs to be valid.
         """
         callsign_valid = bool(self.callsign_text.GetValue().strip())
-        logon_code_valid = bool(self.logon_code_text.GetValue().strip())
 
-        if callsign_valid and logon_code_valid:
-            self.ok_button.Enable()
+        # Check if logon code field is visible
+        if self.logon_code_text.IsShown():
+            # If visible, both callsign and logon code must be valid
+            logon_code_valid = bool(self.logon_code_text.GetValue().strip())
+            if callsign_valid and logon_code_valid:
+                self.ok_button.Enable()
+            else:
+                self.ok_button.Disable()
         else:
-            self.ok_button.Disable()
+            # If hidden, only callsign needs to be valid
+            if callsign_valid:
+                self.ok_button.Enable()
+            else:
+                self.ok_button.Disable()
 
     def get_connection_details(self):
         """
@@ -146,11 +185,20 @@ class ConnectDialog(wx.Dialog):
             tuple: (callsign, logon_code, network_type)
         """
         callsign = self.callsign_text.GetValue().upper()
-        logon_code = self.logon_code_text.GetValue()
+        selection = self.network_radio_box.GetSelection()
 
-        # Get selected network (0 = SayIntentions, 1 = Hoppie)
-        network_type = (
-            "sayintentions" if self.network_radio_box.GetSelection() == 0 else "hoppie"
-        )
+        # Determine network type
+        network_type = "sayintentions" if selection == 0 else "hoppie"
+
+        # Get the appropriate logon code
+        # If the logon code field is hidden, use the saved code from config
+        if not self.logon_code_text.IsShown():
+            if selection == 0:  # SayIntentions
+                logon_code = self.saved_sayintentions_logon_code
+            else:  # Hoppie
+                logon_code = self.saved_hoppie_logon_code
+        else:
+            # Otherwise use the value entered in the field
+            logon_code = self.logon_code_text.GetValue()
 
         return callsign, logon_code, network_type
